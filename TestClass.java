983b052779c47868ffc0b3aa268389767d1ccd37
Seonmi-Hwang <hsm3530@gmail.com>
2020-01-14 14:41:52 +0900

    second test commit

--- TestClass.java
+++ TestClass.java
@@ -1,7 +1,7 @@
 package test;
 
 public class TestClass {
-    public void static main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello!");
     }
 }
